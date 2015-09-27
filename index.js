4f0478d75aa299dfe9fa4cb380e645906bbde07b
George Pimm <jorjpimm@gmail.com>
2015-09-27 11:54:00 +0100

    encrypt and decrypt works

--- index.js
+++ index.js
@@ -1,12 +1,99 @@
+let crypto = require('crypto');
 
-class Secret {
-  constructor() {
-    
+class SecretData
+{
+  constructor(data)
+  {
+    this.data = data;
+  }
+}
+
+class CryptBase
+{
+  constructor(options)
+  {
+    if (options.secrets) {
+      this._secrets = options.secrets;
+    }
+    else if (options.key_split) {
+      this._secrets = [];
+      for (var i = 0; i < options.key_split; ++i) {
+        this._secrets.push(crypto.randomBytes(64).toString('hex'));
+      }
+    }
+  }
+
+  secrets()
+  {
+    return this._secrets;
+  }
+
+  secret_string()
+  {
+    return this.secrets().join("");
+  }
+
+  algorithm()
+  {
+    return 'aes-256-ctr';
+  }
+
+}
+
+class Encryptor extends CryptBase
+{
+  constructor(options)
+  {
+    super(options);
+  }
+
+  encrypt(secret, segments)
+  {
+    let cipher = crypto.createCipher(this.algorithm(), this.secret_string());
+    let crypted = cipher.update(secret.data, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+
+    let output = [];
+    let count = crypted.length / segments;
+    if (count < 1) {
+      throw "Invalid segment count";
+    }
+    for (var i = 0; i < segments; ++i) {
+      let start = i * count;
+      let end = (i != segments-1) ? start + count : crypted.length;
+      output.push(crypted.substring(start, end));
+    }
+    return output;
+  }
+}
+
+class Decryptor extends CryptBase
+{
+  constructor(options)
+  {
+    super(options);
+  }
+
+  decrypt(enc_secret)
+  {
+    let secret_input = enc_secret.join("");
+    var decipher = crypto.createDecipher(this.algorithm(), this.secret_string())
+    var dec = decipher.update(secret_input, 'hex', 'utf8')
+    dec += decipher.final('utf8');
+    return dec;
   }
 }
 
 module.exports = {
-  create: function() {
-    return new Secret();
+  create_secret_data: function(value) {
+    return new SecretData(value);
+  },
+
+  create_encryptor: function(options) {
+    return new Encryptor(options);
+  },
+
+  create_decryptor: function(options) {
+    return new Decryptor(options);
   }
 }
--- tests/basic.js
+++ tests/basic.js
@@ -1,21 +1,33 @@
-var expect = require('chai').expect,
+let expect = require('chai').expect,
     secret = require('../index')
 
 describe('secret', function() {
-  before(function(){
-    // The before() callback gets run before all tests in the suite. Do one-time setup here.
-  });
-  beforeEach(function(){
-    // The beforeEach() callback gets run before each test in the suite.
-  });
-
-  it('provides new secrets', function() {
-    let my_secret = secret.create();
+  it('provides new secret data', function() {
+    let my_secret = secret.create_secret_data("secret pork");
 
     expect(my_secret).to.be.a('Object');
+    expect(my_secret.data).to.equal("secret pork");
   });
 
-  after(function() {
-  	// after() is run after all your tests have completed. Do teardown here.
+  it('provides basic encryption', function() {
+    let encryptor = secret.create_encryptor({
+      key_split: 1
+    });
+    expect(encryptor).to.be.a('Object');
+
+    expect(encryptor.secret_string()).to.be.a('string');
+
+    let my_secret = secret.create_secret_data("secret pork");
+    let encrypted = encryptor.encrypt(my_secret, 2);
+    expect(encrypted).to.be.a('Array');
+    expect(encrypted.length).to.equal(2);
+
+    let decryptor = secret.create_decryptor({
+      secrets: encryptor.secrets()
+    });
+    expect(decryptor).to.be.a('Object');
+
+    let decrypted = decryptor.decrypt(encrypted);
+    expect(decrypted).to.be.equal('secret pork');
   });
 });
